ec7342a25a24608ffc2af93674eb45235c376ab1
Toumi Bechir <bt.toumi@gmail.com>
2020-04-29 01:11:29 +0200

    first template integration

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  get 'pages/home1'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'pages#home'
